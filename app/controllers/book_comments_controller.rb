d362b09e7c817063ffe67fc44b67cabd2bed2ceb
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-05-04 04:22:29 +0000

    [Add]コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,2 +1,28 @@
 class BookCommentsController < ApplicationController
+  def create
+    # binding.pry
+    book=Book.find(params[:book_id])
+    book_comment=current_user.book_comments.new(book_comment_params)
+    book_comment.book_id=book.id
+    # binding.pry
+    if book_comment.save
+      redirect_to request.referer
+    end
+  end
+
+  def destroy
+    # binding.pry
+    book_comment=BookComment.find(params[:id])
+    # binding.pry
+    if book_comment.destroy
+      redirect_to request.referer
+    end
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:body)
+  end
+
 end
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,2 +1,6 @@
 class BookComment < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+
+  validates :body,presence: true
 end
