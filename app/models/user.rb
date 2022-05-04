3e2173445522b6d2cb70af539275baba468c344d
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-05-04 08:50:20 +0000

    [Update]userコントローラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,25 +9,29 @@ class User < ApplicationRecord
   has_many :book_comments,dependent: :destroy
   has_one_attached :profile_image
 
-  has_many :active_relationships,class_name: "relationship",
+  has_many :active_relationships,class_name: "Relationship",
                                 foreign_key: "follower_id",
                                 dependent: :destroy
   has_many :following,through: :active_relationships,source: :followed
 
-  has_many :passive_relationships,class_name:  "relationship",
+  has_many :passive_relationships,class_name:  "Relationship",
                                   foreign_key: "followed_id",
                                   dependent: :destroy
-  has_many :follower,through: :passive_relationships,source: :follower
+  has_many :followers,through: :passive_relationships,source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true,presence: true
   validates :introduction, length: { maximum: 50 }
-  
+
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
   end
-  
+
   def unfollow(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   def get_profile_image
